28b65d8c93bfa66fbf9cf6c062f48ee7d8d734a4
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-10 13:33:49 +0100

    added initial data for our entity computer

---
+++ backend/src/main/kotlin/com/oliverloenning/backend/daos/Computer.kt
@@ -0,0 +1,20 @@
+package com.oliverloenning.backend.daos
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Computer(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long,
+        val computerName: String,
+        val cpu: String,
+        val ram: String,
+        val gpu: String,
+        val hardDrive: String,
+        val computerType: String,
+        val price: Int
+)
