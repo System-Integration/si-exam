616a990e1723d5fc2313e1c65d6621942154cb64
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-11 16:58:15 +0100

    more stuff

--- backend/src/main/kotlin/com/oliverloenning/backend/tools/MyFirstRoute.kt
+++ backend/src/main/kotlin/com/oliverloenning/backend/tools/MyFirstRoute.kt
@@ -1,10 +1,12 @@
 package com.oliverloenning.backend.tools
 
+import org.apache.camel.CamelContext
 import org.apache.camel.builder.RouteBuilder
 import org.springframework.stereotype.Component
 
 @Component
-class MyRoute : RouteBuilder() {
+class MyRoute(context: CamelContext?) : RouteBuilder(context) {
+
 
     @Throws(Exception::class)
     override fun configure() {
