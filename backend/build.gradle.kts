eb30f289cb437513403ce4c743fb5f639bd3b5a4
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-12 10:07:01 +0100

    rabbitmq instead of camel

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -27,13 +27,15 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.apache.camel:camel-spring-boot-starter:2.24.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.amqp:spring-rabbit-test")
 }
+
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
