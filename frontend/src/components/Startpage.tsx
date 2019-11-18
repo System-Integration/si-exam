33164f5b27cb5ad71171b7b8ea64806b4192b9d9
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-18 11:36:01 +0100

    we changed name of frontend page

--- frontend/src/components/Startpage.tsx
+++ frontend/src/components/Startpage.tsx
@@ -3,5 +3,5 @@ import React from "react";
 interface Props {}
 
 export const Startpage: React.FC<Props> = () => {
-  return <div>This is Startpage</div>;
+  return <div>This is Startpageeeee</div>;
 };
