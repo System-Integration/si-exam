510d440955b926d1395543220bc80d09fd8bf71e
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-28 21:48:52 +0100

    next lvl kubernetes with docker-compose

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ if [ "$DOCKER_HUB_PUSH" == "master" ]; then
   docker build -t oliverloenning/si-frontend ./frontend
   docker build -t oliverloenning/si-legacy-backend ./backend
   docker build -t oliverloenning/si-rabbitmq ./rabbitmq
-  ./backend/gradlew jib --image=oliverloenning/si-backend
   docker push oliverloenning/si-frontend
   docker push oliverloenning/si-backend
   docker push oliverloenning/si-rabbitmq
+  cd backend && ./gradlew jib --image=oliverloenning/si-backend
 fi
