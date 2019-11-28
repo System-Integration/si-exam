eb3fed8bfe5d46cfdc7b6de52f4a2d5cffdaa3fa
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-28 21:33:25 +0100

    testing skaffold

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 if [ "$DOCKER_HUB_PUSH" == "master" ]; then
   docker build -t oliverloenning/si-frontend ./frontend
-  docker build -t oliverloenning/si-backend ./backend
+  docker build -t oliverloenning/si-legacy-backend ./backend
   docker build -t oliverloenning/si-rabbitmq ./rabbitmq
+  ./backend/gradlew jib --image=oliverloenning/si-backend
   docker push oliverloenning/si-frontend
   docker push oliverloenning/si-backend
   docker push oliverloenning/si-rabbitmq
