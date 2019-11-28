74a9f62c257e1369723d4a3fdf375e24b4282ed0
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-28 22:00:37 +0100

    small fix

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ "$DOCKER_HUB_PUSH" == "master" ]; then
   docker build -t oliverloenning/si-legacy-backend ./backend
   docker build -t oliverloenning/si-rabbitmq ./rabbitmq
   docker push oliverloenning/si-frontend
-  docker push oliverloenning/si-backend
+  docker push oliverloenning/si-legacy-backend
   docker push oliverloenning/si-rabbitmq
   cd backend && ./gradlew jib --image=oliverloenning/si-backend
 fi
