aee6da6482a0cb44251584a37704edb8e4c73534
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-28 10:25:02 +0100

    cleanup of nginx

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,6 @@
 if [ "$DOCKER_HUB_PUSH" == "master" ]; then
   docker build -t oliverloenning/si-frontend ./frontend
   docker build -t oliverloenning/si-backend ./backend
-  docker build -t oliverloenning/si-nginx ./nginx
   docker push oliverloenning/si-frontend
   docker push oliverloenning/si-backend
-  docker push oliverloenning/si-nginx
 fi
