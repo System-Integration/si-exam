f2eebe8a1169bc6287d09c02d20502594a698afb
OliverLoenning <Oliver.S.Loenning@gmail.com>
2019-11-30 22:16:27 +0100

    try to fix

--- backend/src/main/kotlin/com/oliverloenning/backend/tools/MqttClientConfig.kt
+++ backend/src/main/kotlin/com/oliverloenning/backend/tools/MqttClientConfig.kt
@@ -4,6 +4,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient
 import org.eclipse.paho.client.mqttv3.MqttClientPersistence
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
@@ -16,13 +17,12 @@ class MqttClientConfig(private val context: ApplicationContext) {
     @Bean
     fun customMqttClient(): MqttClient {
         val env = System.getenv("MOM_SOCKET_IP") ?: "mom"
-        val mqttClient = MqttClient("ws://$env:15675/ws", "Server", MqttDefaultFilePersistence())
+        val mqttClient = MqttClient("ws://$env:15675/ws", "Server", MemoryPersistence())
 
         val options = MqttConnectOptions()
         options.isAutomaticReconnect=true
         options.isCleanSession=true
         options.connectionTimeout=10
-
         mqttClient.connect(options)
 
         return mqttClient
