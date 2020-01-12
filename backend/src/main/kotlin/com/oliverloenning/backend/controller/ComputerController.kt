91fca73fcdf249a5b104f298e7730a3342c2f111
unknown <ullhaq@hotmail.com>
2020-01-12 10:13:06 +0100

    get item by ID added to api

--- backend/src/main/kotlin/com/oliverloenning/backend/controller/ComputerController.kt
+++ backend/src/main/kotlin/com/oliverloenning/backend/controller/ComputerController.kt
@@ -2,13 +2,15 @@ package com.oliverloenning.backend.controller
 
 import com.oliverloenning.backend.database.daos.Computer
 import com.oliverloenning.backend.logic.ComputerFacade
-import com.oliverloenning.backend.logic.MonitorFacade
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ComputerController(private val computerFacade: ComputerFacade, private val monitorFacade: MonitorFacade) {
-
+class ComputerController(private val computerFacade: ComputerFacade) {
     @RequestMapping("/computers")
     fun computers(): List<Computer> = computerFacade.getAllComputers()
+    
+    @RequestMapping("/computers/{id}")
+    fun getComputer(@PathVariable id: Long) = computerFacade.findComputer(id)
 }
